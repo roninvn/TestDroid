36b0e6081189bc9658f7d2476ba67abb8ff5cde3
Nam <Nam@Nam-PC>
2011-09-25 18:18:10 +0700

    Change-Id: I353011cca8e357f0be07d7abfefd5b87a14ca05e Signed-off-by: Nam <Nam@Nam-PC>

---
+++ src/com/name/testdroid/TestDroidActivity.java
@@ -0,0 +1,13 @@
+package com.name.testdroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestDroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
